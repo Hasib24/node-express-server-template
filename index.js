828c17f1c09f60b0333d15fa7e30e5363a9ab55f
Hasib24 <hasibislam1488@gmail.com>
2023-05-23 14:41:11 +0600

    install and use dotenv

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
+require('dotenv').config()
 
 //Middleware
 app.use(cors())
 app.use(express.json())
 
+
+const envExample = process.env.USER;
+console.log(envExample);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
